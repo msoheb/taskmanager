60718ed6fd2d6eb312089776d909810388251535
msoheb <soheb.mukadam@gmail.com>
2026-01-29 18:11:55 +0000

    feat: add Category entity with unique name constraint

---
+++ src/main/java/com/shoyu/taskmanager/entity/Category.java
@@ -0,0 +1,36 @@
+package com.shoyu.taskmanager.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "categories",
+    uniqueConstraints = {
+        @UniqueConstraint(name = "uk_category_name", columnNames = {"name"})
+    }
+)
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column
+    private String description;
+
+}
