19e876ab1585529fce6d2ff9684d5c10e0e8c7d7
msoheb <soheb.mukadam@gmail.com>
2026-01-30 03:36:20 +0000

    feat: add CategoryRepository with findByName query

---
+++ src/main/java/com/shoyu/taskmanager/repository/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.shoyu.taskmanager.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.shoyu.taskmanager.entity.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByName(String name);
+}
